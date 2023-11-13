1f423bd6d7eeeb94b0b4cddfb82c003e4fd94b9b
pildrums <there7788@gmail.com>
2023-11-13 19:13:29 +0900

    Chore: 초기 페이지를 오늘의 승부예측 페이지로 리다이렉트 처리

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/match/today",
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/match/today",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
