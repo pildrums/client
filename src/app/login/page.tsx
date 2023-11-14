fd5fbe5514f8398ed681d2f38fbeba74ec78841c
pildrums <there7788@gmail.com>
2023-11-14 12:43:22 +0900

    Feat: title, meta 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
-"use client";
-
 import Link from "next/link";
 
 function LoginPage() {
   return (
     <>
+      <head>
+        <title>승리요정 - 로그인</title>
+        <meta name="description" content="승리요정 로그인" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </head>
       <Link href="/">홈으로</Link>
     </>
   );
