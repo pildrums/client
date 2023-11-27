18f2501596bcd67e44c0e84b9bce68edc2422601
pildrums <there7788@gmail.com>
2023-11-27 18:14:08 +0900

    Design: GlobalStyles.scss 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/common/Header";
 import StyledComponentsRegistry from "@/lib/registry";
 import { Metadata } from "next";
 import { ReactNode } from "react";
+import "@/styles/GlobalStyles.scss";
 
 export const metadata: Metadata = {
   title: "승리요정",
