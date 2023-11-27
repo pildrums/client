4e2519972c2cc06c67e193528bf2ce9f1425db02
pildrums <there7788@gmail.com>
2023-11-27 21:02:54 +0900

    Fix: scss.module 적용 완료

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,110 +2,54 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { css, styled } from "styled-components";
 import SubMenu from "./SubMenu";
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+
+const cn = classNames.bind(styles);
 
 function Header() {
   const pathname = usePathname();
 
   return (
     <>
-      <HeaderBlock>
-        <HeaderContent>
+      <header className={styles.header_block}>
+        <div className={styles.content}>
           <h1>
-            <Logo href="/match/today">승리요정</Logo>
+            <Link className={styles.logo} href="/match/today">
+              승리요정
+            </Link>
           </h1>
-          <Navigation>
-            <ul>
-              <NavItem
+          <nav className={styles.navigation}>
+            <div>
+              <Link
                 href="/match/today"
-                $today={pathname === "/match/today"}
-                $month={pathname === "/match/month"}
+                className={cn(
+                  `${pathname === "/match/today" ? styles.active_page : ""}`,
+                  `${pathname === "/match/month" ? styles.active_page : ""}`,
+                )}
               >
                 승부예측
-              </NavItem>
-              <NavItem href="/community" $community={pathname === "/community"}>
+              </Link>
+              <Link
+                className={cn(
+                  `${pathname === "/community" ? styles.active_page : ""}`,
+                )}
+                href="/community"
+              >
                 커뮤니티
-              </NavItem>
-            </ul>
-          </Navigation>
-          <Login href="/login">로그인</Login>
-        </HeaderContent>
+              </Link>
+            </div>
+          </nav>
+          <Link className={styles.login} href="/login">
+            로그인
+          </Link>
+        </div>
         {pathname === "/community" || <SubMenu />}
-      </HeaderBlock>
-      <Space />
+      </header>
+      <div className={styles.space} />
     </>
   );
 }
-
-const HeaderBlock = styled.header`
-  width: 100%;
-  height: 50px;
-  background: #ccc;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 20px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 9999;
-`;
-
-const HeaderContent = styled.div`
-  width: 1280px;
-  height: inherit;
-  background: coral;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Logo = styled(Link)`
-  background: #999;
-  padding: 8px 16px;
-`;
-
-const Navigation = styled.nav`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ul {
-    display: flex;
-    gap: 80px;
-  }
-`;
-
-const NavItem = styled(Link)<{
-  $today?: boolean;
-  $month?: boolean;
-  $community?: boolean;
-}>`
-  ${(props) =>
-    props.$today &&
-    css`
-      font-weight: 700;
-    `}
-  ${(props) =>
-    props.$month &&
-    css`
-      font-weight: 700;
-    `}
-  ${(props) =>
-    props.$community &&
-    css`
-      font-weight: 700;
-    `}
-`;
-
-const Login = styled(Link)`
-  background: #999;
-  padding: 8px 16px;
-  transition: background-color 0.2s ease-in-out;
-`;
-
-const Space = styled.div`
-  margin-bottom: 100px;
-`;
 
 export default Header;
--- src/components/common/SubMenu.tsx
+++ src/components/common/SubMenu.tsx
@@ -3,7 +3,10 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
-import { styled } from "styled-components";
+import classNames from "classnames/bind";
+import styles from "./SubMenu.module.scss";
+
+const cn = classNames.bind(styles);
 
 function SubMenu() {
   const [position, setPosition] = useState(0);
@@ -25,65 +28,30 @@ function SubMenu() {
   }, [onScroll]);
 
   return (
-    <SubMenuBlock className={visible ? "scrollUp" : "scrollDown"}>
-      <SubMenuItem className={pathname === "/match/month" ? "active" : ""}>
+    <ul
+      className={cn(
+        `${styles.submenu_block}`,
+        visible ? `${styles.scrollUp}` : `${styles.scrollDown}`,
+      )}
+    >
+      <li
+        className={cn(
+          `${styles.submenu_item}`,
+          pathname === "/match/month" ? `${styles.active}` : "",
+        )}
+      >
         <Link href="/match/month">월간 승리요정</Link>
-      </SubMenuItem>
-      <SubMenuItem className={pathname === "/match/today" ? "active" : ""}>
+      </li>
+      <li
+        className={cn(
+          `${styles.submenu_item}`,
+          pathname === "/match/today" ? `${styles.active}` : "",
+        )}
+      >
         <Link href="/match/today">오늘의 승부예측</Link>
-      </SubMenuItem>
-    </SubMenuBlock>
+      </li>
+    </ul>
   );
 }
-
-const SubMenuBlock = styled.ul`
-  position: absolute;
-  z-index: 999;
-  top: 70px;
-  left: 230px;
-  display: flex;
-  gap: 20px;
-  &.scrollDown {
-    transition: opacity 0.2s ease-in-out;
-    animation: scrollDown 0.2s normal;
-    opacity: 0;
-    transform: translateY(-70px);
-    z-index: 999;
-    @keyframes scrollDown {
-      0% {
-        transform: translateY(0);
-      }
-      100% {
-        transform: translateY(-20px);
-      }
-    }
-  }
-  &.scrollUp {
-    transition: opacity 0.2s ease-in-out;
-    animation: scrollUp 0.2s normal;
-    opacity: 1;
-    transform: translateY(0);
-    z-index: 999;
-    @keyframes scrollUp {
-      0% {
-        transform: translateY(-20px);
-      }
-      100% {
-        transform: translateY(0);
-      }
-    }
-  }
-`;
-
-const SubMenuItem = styled.li`
-  width: 120px;
-  text-align: center;
-  cursor: pointer;
-  &.active {
-    font-weight: 700;
-    border-bottom: 4px solid #000;
-    padding-bottom: 6px;
-  }
-`;
 
 export default SubMenu;
