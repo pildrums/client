24216f1f0a212730b0e2ec7f4b9e2736a7555272
pildrums <there7788@gmail.com>
2023-11-28 19:18:06 +0900

    Feat: 공용 fetch 함수 및 인터셉터 적용 - 공용으로 api 연동할 수 있는 함수 작성 => 각 api 함수는 fetchData 함수로 작성! - http 상태 별 메시지 인터셉터 처리(데이터를 요청하거나 응답을 받기 전에 처리) - 쿠키 인터셉터 처리

---
+++ src/lib/api/commonAPI.ts
@@ -0,0 +1,51 @@
+import { getAccessTokenCookie } from "../etc/cookies";
+import { instance } from "./instance";
+
+type Method = "get" | "post" | "put" | "delete";
+
+// 공용 fetch 함수
+export const fetchData = async (
+  url: string,
+  method: Method,
+  reqData?: unknown,
+) => {
+  try {
+    const { data } = await instance({ url, method, data: reqData });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// interceptor
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getAccessTokenCookie();
+    if (!accessToken) {
+      return config;
+    }
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  function (config) {
+    if (config.status === 404) {
+      console.log("404 Not Found");
+    } else if (config.status === 403) {
+      console.log("403 Forbidden");
+    } else if (config.status === 401) {
+      console.log("401 Unauthorized");
+    } else if (config.status === 500) {
+      console.log("500 Internal Error");
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
