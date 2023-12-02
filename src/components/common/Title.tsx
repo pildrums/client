147fe4336e8ca4ada49561da01aea94a9ac53396
pildrums <there7788@gmail.com>
2023-12-03 05:34:04 +0900

    Chore: JSDoc으로 각 컴포넌트 주석 추가

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -5,18 +5,38 @@ interface TitleProps {
   children: ReactNode;
 }
 
+/**
+ * @description 48px / bold(700)
+ * @param children
+ * @returns Title1 컴포넌트
+ */
 function Title1({ children }: TitleProps) {
   return <h1 className="title_1">{children}</h1>;
 }
 
+/**
+ * @description 36px / semi-bold(600)
+ * @param children
+ * @returns Title2 컴포넌트
+ */
 function Title2({ children }: TitleProps) {
   return <h2 className="title_2">{children}</h2>;
 }
 
+/**
+ * @description 24px / semi-bold(600)
+ * @param children
+ * @returns Title3 컴포넌트
+ */
 function Title3({ children }: TitleProps) {
   return <h3 className="title_3">{children}</h3>;
 }
 
+/**
+ * @description 22px / medium(500)
+ * @param children
+ * @returns Title4 컴포넌트
+ */
 function Title4({ children }: TitleProps) {
   return <h4 className="title_4">{children}</h4>;
 }
