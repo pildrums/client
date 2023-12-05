eb40f90bc921ff73381896e5f54efb86ce74ef1d
pildrums <there7788@gmail.com>
2023-12-05 17:49:57 +0900

    Fix: 페이지 별 타이틀 메타데이터 적용

--- src/app/match/today/page.tsx
+++ src/app/match/today/page.tsx
@@ -1,7 +1,10 @@
-"use client";
-
 import DateSection from "@/components/today/DateSection";
 import ScoreBoard from "@/components/today/ScoreBoard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "플레이닷 | 오늘의 승부예측",
+};
 
 function TodayPage() {
   return (
