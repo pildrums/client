a29e5f2bb76bdb3e1130fcdbaa272bc07bffc2b8
pildrums <there7788@gmail.com>
2023-12-05 16:16:38 +0900

    Fix: 클래스 이름 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/common/Header";
 import { Metadata } from "next";
 import { ReactNode } from "react";
 import "@/styles/GlobalStyles.scss";
-import styles from "./layout.module.scss";
 
 export const metadata: Metadata = {
   title: "승리요정",
@@ -15,7 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <html lang="ko">
         <body>
           <Header />
-          <div className={styles.layout}>{children}</div>
+          <div>{children}</div>
         </body>
       </html>
     </>
