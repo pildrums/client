deb1632c033db239eddeee0829490c06b5269e14
pildrums <there7788@gmail.com>
2023-12-07 00:45:32 +0900

    Fix: today-page-block 클래스 네임 추가

--- src/app/match/today/page.tsx
+++ src/app/match/today/page.tsx
@@ -1,6 +1,7 @@
 import DateSection from "@/components/today/DateSection";
 import ScoreList from "@/components/today/ScoreList";
 import { Metadata } from "next";
+import "./TodayPage.scss";
 
 export const metadata: Metadata = {
   title: "플레이닷 | 오늘의 승부예측",
@@ -8,10 +9,10 @@ export const metadata: Metadata = {
 
 function TodayPage() {
   return (
-    <>
+    <div className="today-page-block">
       <DateSection />
       <ScoreList />
-    </>
+    </div>
   );
 }
 
