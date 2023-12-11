136f1f91e5a270364d93028f865728e93449ae02
pildrums <there7788@gmail.com>
2023-12-12 06:16:17 +0900

    Refactor: 컴포넌트 하나로 통일 - 기존 Text1, Text2... 로 되어있던 컴포넌트를 Text로 하나로 합침. - className에 OR 연산자 적용 - interface에 text1, text2, text3, captionText 추가(optional chaining)

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -3,42 +3,16 @@ import { ReactNode } from "react";
 
 interface TextProps {
   children: ReactNode;
+  text1?: string;
+  text2?: string;
+  text3?: string;
+  captionText?: string;
 }
 
-/**
- * @description 18px / medium(500)
- * @param children
- * @returns Text1 컴포넌트
- */
-function Text1({ children }: TextProps) {
-  return <span className="text_1">{children}</span>;
+function Text({ children, text1, text2, text3, captionText }: TextProps) {
+  return (
+    <span className={text1 || text2 || text3 || captionText}>{children}</span>
+  );
 }
 
-/**
- * @description 16px / medium(500)
- * @param children
- * @returns Text2 컴포넌트
- */
-function Text2({ children }: TextProps) {
-  return <span className="text_2">{children}</span>;
-}
-
-/**
- * @description 14px / medium(500)
- * @param children
- * @returns Text3 컴포넌트
- */
-function Text3({ children }: TextProps) {
-  return <span className="text_3">{children}</span>;
-}
-
-/**
- * @description 12px / normal(400)
- * @param children
- * @returns Caption 컴포넌트
- */
-function Caption({ children }: TextProps) {
-  return <span className="caption_text">{children}</span>;
-}
-
-export { Text1, Text2, Text3, Caption };
+export default Text;
