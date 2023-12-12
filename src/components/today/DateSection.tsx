49eda8b23a071b7a0d55b3d0e4340e0dd296edda
pildrums <there7788@gmail.com>
2023-12-13 03:20:48 +0900

    Fix: 기존 Text1 컴포넌트를 Title 컴포넌트로 변경

--- src/components/today/DateSection.tsx
+++ src/components/today/DateSection.tsx
@@ -1,5 +1,5 @@
 import Text from "../common/Text";
-import { Title1, Title2, Title3, Title4 } from "../common/Title";
+import Title from "../common/Title";
 import "./DateSection.scss";
 
 function DateSection() {
@@ -7,11 +7,11 @@ function DateSection() {
     <section className="date-block">
       <div className="date-block__title">
         <span className="live">LIVE</span>
-        <Title2>12월 4일</Title2>
+        <Title large>12월 4일</Title>
       </div>
       <div className="date-block__text">
-        <Text text2="text-2">투표는 12/4 오후 7시까지 가능합니다.</Text>
-        <Text text2="text-2">투표를 통해 우리팀을 응원해주세요!</Text>
+        <Text medium>투표는 12/4 오후 7시까지 가능합니다.</Text>
+        <Text medium>투표를 통해 우리팀을 응원해주세요!</Text>
       </div>
     </section>
   );
