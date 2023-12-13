27356e3f463396a7e0fbcc53de5ecb13e467ee12
doitidey <evxrafter@gmail.com>
2023-12-13 22:29:17 +0900

    Feat: 채팅헤더 컴포넌트 생성

---
+++ src/components/chat/ChatHeader.tsx
@@ -0,0 +1,22 @@
+import "./ChatHeader.scss";
+
+function ChatHeader() {
+  return (
+    <div className="chat-header">
+      <div className="chat-header__button">*</div>
+      <div className="chat-header__score">3</div>
+      <div className="chat-header__info">
+        <div className="date">23.11.23</div>
+        <div className="teams">
+          <div className="teams__name">기아</div>
+          <div className="teams__vs">VS</div>
+          <div className="teams__name">키움</div>
+        </div>
+      </div>
+      <div className="chat-header__score">5</div>
+      <div className="chat-header__button">*</div>
+    </div>
+  );
+}
+
+export default ChatHeader;
