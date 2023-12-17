7601df71de7d0b834b117cad8797c7439bf008fd
pildrums <there7788@gmail.com>
2023-12-18 03:18:07 +0900

    Fix: 텍스트 스토리 Large, Medium... 으로 이름 변경

--- src/components/common/Text.stories.tsx
+++ src/components/common/Text.stories.tsx
@@ -3,11 +3,38 @@ import Text from "./Text";
 
 const meta: Meta<typeof Text> = {
   component: Text,
+  argTypes: {
+    large: { control: true },
+    medium: { control: true },
+    small: { control: true },
+    caption: { control: true },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Text>;
 
-export const TextFirstStory: Story = {
-  args: {},
+export const Large: Story = {
+  args: {
+    children: "Large",
+    large: true,
+  },
+};
+export const Medium: Story = {
+  args: {
+    children: "Medium",
+    medium: true,
+  },
+};
+export const Small: Story = {
+  args: {
+    children: "Small",
+    small: true,
+  },
+};
+export const Caption: Story = {
+  args: {
+    children: "Caption",
+    caption: true,
+  },
 };
