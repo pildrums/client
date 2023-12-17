1eddbb6bf341611db6e5fb6d647518208a51fb2f
doitidey <evxrafter@gmail.com>
2023-12-17 22:07:16 +0900

    Feat: 메뉴 모달 컴포넌트 생성

---
+++ src/components/chat/modals/MenuModal.tsx
@@ -0,0 +1,24 @@
+import "./MenuModal.scss";
+
+function MenuModal() {
+  return (
+    <>
+      <div className="menu-container">
+        <div className="menu-container__box">
+          <div>사진</div>
+          <p>미니투표</p>
+        </div>
+        <div className="menu-container__box">
+          <div>사진</div>
+          <p>외치기</p>
+        </div>
+        <div className="menu-container__box">
+          <div>사진</div>
+          <p>선물하기</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MenuModal;
