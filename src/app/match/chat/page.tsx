5c202113ab82bc3a52fbf97f1d4c980c6467d95c
doitidey <evxrafter@gmail.com>
2023-12-17 22:08:47 +0900

    Refactor: 채팅 인풋, 모달 Floating Area에서 관리

--- src/app/match/chat/page.tsx
+++ src/app/match/chat/page.tsx
@@ -1,8 +1,8 @@
-import ChatHeader from "@/components/chat/ChatHeader";
-import ChatInput from "@/components/chat/ChatInput";
-import "./page.scss";
 import { Metadata } from "next";
+import "./page.scss";
+import ChatHeader from "@/components/chat/ChatHeader";
 import ChatLog from "@/components/chat/chatlog/ChatLog";
+import FloatingArea from "@/components/chat/FloatingArea";
 
 export const metadata: Metadata = {
   title: "플레이닷 | 채팅",
@@ -11,9 +11,9 @@ export const metadata: Metadata = {
 function ChatPage() {
   return (
     <div className="chat">
+      <FloatingArea />
       <ChatHeader />
       <ChatLog />
-      <ChatInput />
     </div>
   );
 }
---
+++ src/components/chat/FloatingArea.tsx
@@ -0,0 +1,14 @@
+import "./FloatingArea.scss";
+import MenuModal from "@/components/chat/modals/MenuModal";
+import ChatInput from "@/components/chat/ChatInput";
+
+function FloatingArea() {
+  return (
+    <div className="floatingarea">
+      <ChatInput />
+      <MenuModal />
+    </div>
+  );
+}
+
+export default FloatingArea;
