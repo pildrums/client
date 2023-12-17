847d6aaf6b62b87cfe69a1528b1b2ad503055a47
pildrums <there7788@gmail.com>
2023-12-18 03:25:20 +0900

    Feat: 현재 경기 목데이터 및 map으로 자동으로 표현

--- src/components/today/ScoreList.tsx
+++ src/components/today/ScoreList.tsx
@@ -1,13 +1,41 @@
 import "./ScoreList.scss";
 import ScoreListItem from "./ScoreListItem";
 
+const team = [
+  {
+    id: 1,
+    home: "삼성 라이온즈",
+    away: "기아 타이거즈",
+  },
+  {
+    id: 2,
+    home: "LG 트윈스",
+    away: "두산 베어스",
+  },
+  {
+    id: 3,
+    home: "롯데 자이언츠",
+    away: "NC 다이노스",
+  },
+  {
+    id: 4,
+    home: "SSG 랜더스",
+    away: "KT 위즈",
+  },
+  {
+    id: 5,
+    home: "한화 이글스",
+    away: "키움 히어로즈",
+  },
+];
+
 function ScoreList() {
   return (
     <section className="score-block">
       <ul className="score-block__content">
-        <ScoreListItem />
-        <ScoreListItem />
-        <ScoreListItem />
+        {team.map((item) => (
+          <ScoreListItem key={item.id} home={item.home} away={item.away} />
+        ))}
       </ul>
     </section>
   );
