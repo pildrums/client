993ff8e7d93198ebb360530320db40c986e01e67
pildrums <there7788@gmail.com>
2023-12-19 00:32:51 +0900

    Feat: 홈팀 로고 및 원정팀 로고 경로 지정 - 배열 데이터 이름 match로 변경 - 데이터에 homeImg, awayImg 추가 및 ScoreListItem으로 전달

--- src/components/today/ScoreList.tsx
+++ src/components/today/ScoreList.tsx
@@ -1,31 +1,41 @@
 import "./ScoreList.scss";
 import ScoreListItem from "./ScoreListItem";
 
-const team = [
+const match = [
   {
     id: 1,
     home: "삼성 라이온즈",
     away: "기아 타이거즈",
+    homeImg: "/images/lions.svg",
+    awayImg: "/images/tigers.svg",
   },
   {
     id: 2,
     home: "LG 트윈스",
     away: "두산 베어스",
+    homeImg: "/images/twins.svg",
+    awayImg: "/images/bears.svg",
   },
   {
     id: 3,
     home: "롯데 자이언츠",
     away: "NC 다이노스",
+    homeImg: "/images/giants.svg",
+    awayImg: "/images/dinos.svg",
   },
   {
     id: 4,
     home: "SSG 랜더스",
     away: "KT 위즈",
+    homeImg: "/images/landers.svg",
+    awayImg: "/images/wiz.svg",
   },
   {
     id: 5,
     home: "한화 이글스",
     away: "키움 히어로즈",
+    homeImg: "/images/eagles.svg",
+    awayImg: "/images/heros.svg",
   },
 ];
 
@@ -33,8 +43,14 @@ function ScoreList() {
   return (
     <section className="score-block">
       <ul className="score-block__content">
-        {team.map((item) => (
-          <ScoreListItem key={item.id} home={item.home} away={item.away} />
+        {match.map((item) => (
+          <ScoreListItem
+            key={item.id}
+            home={item.home}
+            away={item.away}
+            homeImg={item.homeImg}
+            awayImg={item.awayImg}
+          />
         ))}
       </ul>
     </section>
