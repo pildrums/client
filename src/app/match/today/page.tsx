5641196f83ca0bcab7d8efc718e9fa2ac97d560c
pildrums <there7788@gmail.com>
2023-12-20 03:16:35 +0900

    Feat: 댓글 컴포넌트 렌더링

--- src/app/match/today/page.tsx
+++ src/app/match/today/page.tsx
@@ -2,6 +2,7 @@ import DateSection from "@/components/today/DateSection";
 import ScoreList from "@/components/today/ScoreList";
 import { Metadata } from "next";
 import "./TodayPage.scss";
+import Reply from "@/components/common/Reply";
 
 export const metadata: Metadata = {
   title: "플레이닷 | 오늘의 승부예측",
@@ -12,6 +13,7 @@ function TodayPage() {
     <div className="today-page-block">
       <DateSection />
       <ScoreList />
+      <Reply />
     </div>
   );
 }
