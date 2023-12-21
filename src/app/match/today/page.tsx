c498a22d4f217443fc202b6d048652cbb211ae42
pildrums <there7788@gmail.com>
2023-12-21 20:44:52 +0900

    Fix: Reply 컴포넌트 경로 변경

--- src/app/match/today/page.tsx
+++ src/app/match/today/page.tsx
@@ -2,7 +2,8 @@ import DateSection from "@/components/today/DateSection";
 import ScoreList from "@/components/today/ScoreList";
 import { Metadata } from "next";
 import "./TodayPage.scss";
-import Reply from "@/components/common/Reply";
+import Reply from "@/components/reply/Reply";
+
 
 export const metadata: Metadata = {
   title: "플레이닷 | 오늘의 승부예측",
