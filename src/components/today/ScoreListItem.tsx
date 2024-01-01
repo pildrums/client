26b5ce3545db9ded67988f4486103f89d69544bc
pildrums <there7788@gmail.com>
2024-01-01 13:18:26 +0900

    Feat: 경기 시간 추가 - Title 컴포넌트에 small 속성 적용 - 현재 날짜, 요일을 변수에 담음 - class 이름에 match-day 적용

--- src/components/today/ScoreListItem.tsx
+++ src/components/today/ScoreListItem.tsx
@@ -2,10 +2,10 @@
 
 import { useCallback, useState } from "react";
 import Text from "../common/Text";
-import Title from "../common/Title";
 import "./ScoreListItem.scss";
 import classNames from "classnames";
 import Image from "next/image";
+import Title from "../common/Title";
 
 interface ScoreListItemProps {
   home: string;
@@ -14,55 +14,64 @@ interface ScoreListItemProps {
   awayImg: string;
 }
 
+const today = new Date();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+const week = ["일", "월", "화", "수", "목", "금", "토"];
+// time은 실제 경기시간 데이터를 불러올 예정(목 데이터)
+const time = "17:00";
+const matchDay = `${month}월 ${date}일 (${week[today.getDay()]}) ${time}`;
+
 function ScoreListItem({ home, away, homeImg, awayImg }: ScoreListItemProps) {
-  // Todo: left, right를 selectTeam으로 리팩토링
   const [selectHome, setSelectHome] = useState(false);
   const [selectAway, setSelectAway] = useState(false);
-  const [average, setAverage] = useState(false);
 
   const onClickLeft = useCallback(() => {
     setSelectHome(!selectHome);
     setSelectAway(false);
-    setAverage(true);
   }, [selectHome]);
 
   const onClickRight = useCallback(() => {
     setSelectAway(!selectAway);
     setSelectHome(false);
-    setAverage(true);
   }, [selectAway]);
 
   return (
-    <li className="score-item-block">
-      <div
-        className={classNames(
-          `score-item-block__left ${selectHome ? "active-left" : ""}`,
-        )}
-        onClick={onClickLeft}
-      >
-        <div className="team-img">
-          <Image src={homeImg} alt="lions" width={55} height={55} />
+    <>
+      <Title className="match-day" small>
+        {matchDay}
+      </Title>
+      <li className="score-item-block">
+        <div
+          className={classNames(
+            `score-item-block__left ${selectHome ? "active-left" : ""}`,
+          )}
+          onClick={onClickLeft}
+        >
+          <div className="team-img">
+            <Image src={homeImg} alt="lions" width={55} height={55} />
+          </div>
+          <div className="team-info-left">
+            {selectHome ? <Text large>{home}</Text> : <Text large>{home}</Text>}
+            <Title medium>50%</Title>
+          </div>
         </div>
-        <div className="team-info-left">
-          {selectHome ? <Text small>{home}</Text> : <Text large>{home}</Text>}
-          {average && <Title medium>50%</Title>}
+        <div
+          className={classNames(
+            `score-item-block__right ${selectAway ? "active-right" : ""}`,
+          )}
+          onClick={onClickRight}
+        >
+          <div className="team-img">
+            <Image src={awayImg} alt="lions" width={55} height={55} />
+          </div>
+          <div className="team-info-right">
+            {selectAway ? <Text small>{away}</Text> : <Text large>{away}</Text>}
+            <Title medium>50%</Title>
+          </div>
         </div>
-      </div>
-      <div
-        className={classNames(
-          `score-item-block__right ${selectAway ? "active-right" : ""}`,
-        )}
-        onClick={onClickRight}
-      >
-        <div className="team-img">
-          <Image src={awayImg} alt="lions" width={55} height={55} />
-        </div>
-        <div className="team-info-right">
-          {selectAway ? <Text small>{away}</Text> : <Text large>{away}</Text>}
-          {average && <Title medium>50%</Title>}
-        </div>
-      </div>
-    </li>
+      </li>
+    </>
   );
 }
 
