18367c64e45c1715c04c8a3db90bfcb7bf9f7f55
doitidey <evxrafter@gmail.com>
2024-01-11 00:21:57 +0900

    Feat: 로그인 페이지 재구성

--- src/components/login/LoginSection.tsx
+++ src/components/login/LoginSection.tsx
@@ -1,23 +1,23 @@
-import GuestButton from "./GuestButton";
+import Title from "../common/Title";
+
 import "./LoginSection.scss";
-import PageNumbers from "./PageNumbers";
-import SocialButtons from "./SocialButtons";
+import Image from "next/image";
 
 function LoginSection() {
   return (
     <div className="login-block">
       <div className="login-content">
-        <PageNumbers />
-        <h1 className="login-title">로그인</h1>
-        <p className="login-description">
-          지금 바로 가입 후, 좋아하는 구단의 승부를 예측해보세요!
-        </p>
-        <div className="login-buttons">
-          <GuestButton />
-          <SocialButtons />
+        <Title largest>플레이닷 ID 생성</Title>
+        <div className="login-content__text">
+          <Title small>간편로그인을 사용하여 아이디,비밀번호 입력 없이</Title>
+          <Title small>쉽게 플레이닷 서비스를 이용할 수 있어!</Title>
         </div>
       </div>
-      <div className="login-image" />
+      <div className="login-buttons">
+        <div className="login-buttons__button">네이버 로그인</div>
+        <div className="login-buttons__button">카카오 로그인</div>
+        <div className="login-buttons__button">구글 로그인</div>
+      </div>
     </div>
   );
 }
